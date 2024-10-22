4c244a320e72e69eadfd3b1aed970a1dd7297580
J0hn23 <jonathan.ortega.bravo@hotmail.com>
2024-10-22 10:34:19 +0200

    Primer commit realizado por mi

---
+++ src/main/java/ortegabravo/maxsport/MaXsport.java
@@ -0,0 +1,16 @@
+
+
+package ortegabravo.maxsport;
+
+/**
+ *
+ * author
+ */
+
+
+public class MaXsport {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
